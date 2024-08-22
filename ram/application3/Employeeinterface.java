ab4c35fd78bbd91bf1ee09018cd8457f4996aa5f
Hameed-85 <148732274+Hameed-85@users.noreply.github.com>
2024-08-22 17:37:52 +0530

    Delete ram directory

--- ram/application3/Employeeinterface.java
+++
@@ -1,8 +0,0 @@
-package com.evergent.corejava.ram.application3;
-
-public interface Employeeinterface {
-	public void employeeInfo(int eno,float sal) throws Exception;
-	public void employeeAddress(String add);
-	public void employeeDetails();
-
-}
